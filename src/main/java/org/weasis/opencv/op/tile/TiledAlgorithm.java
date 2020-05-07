f00f7446b0eec391ded217aec1a2ec7ab22db7a9
Nicolas Roduit <nicolas.roduit@gmail.com>
2020-05-07 15:29:00 +0200

    Update copyright header

--- src/main/java/org/weasis/opencv/op/tile/TiledAlgorithm.java
+++ src/main/java/org/weasis/opencv/op/tile/TiledAlgorithm.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2009-2020 Weasis Team and other contributors.
  *
  * This program and the accompanying materials are made available under the
@@ -6,7 +6,8 @@
  * http://www.eclipse.org/legal/epl-2.0.
  *
  * SPDX-License-Identifier: EPL-2.0
- *******************************************************************************/
+ */
+
 package org.weasis.opencv.op.tile;
 
 import org.opencv.core.Core;
