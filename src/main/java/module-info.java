55fae8727751d65283aff916adbd272702392d33
Nicolas Roduit <nicolas.roduit@gmail.com>
2020-12-06 17:39:51 +0100

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module weasis.core.img {
+  requires java.desktop;
+  requires org.slf4j;
+  requires java.logging;
+
+  exports org.weasis.core.util;
+  exports org.weasis.opencv.data;
+  exports org.weasis.opencv.op;
+  exports org.weasis.opencv.op.lut;
+  exports org.weasis.opencv.op.tile;
+
+  exports org.opencv.core;
+  exports org.opencv.imgcodecs;
+  exports org.opencv.img_hash;
+  exports org.opencv.imgproc;
+  exports org.opencv.osgi;
+  exports org.opencv.utils;
+}
